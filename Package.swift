257f074feaddf64312a43be34f1d4a7df221ff09
Bobby Skinner <bobby.skinner@crosscomm.net>
2016-11-08 19:58:34 -0500

    version change

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@ import PackageDescription
 let urls = ["https://github.com/crosscodersdev/SQLite.swift.git"]
 
 let package = Package(
-	name: "SQLite",
+	name: "SQLite.swift",
 	targets: [
 		Target(name: "SQLite iOS", dependencies: [])
 	],
-	dependencies: urls.map { .Package(url: $0, majorVersion: 0, minor: 90) },
+	dependencies: urls.map { .Package(url: $0, majorVersion: 0, minor: 90 patch: 0) },
 	exclude: []
 )
